30eeeeea4b7b002423abb4d4855300595d3ffaf8
QiLei <8971460+qilei123@user.noreply.gitee.com>
2021-12-19 15:44:07 +0800

    link Database

---
+++ database.py
@@ -0,0 +1,41 @@
+import json
+import pymysql
+import os
+import time
+
+videos_list = os.listdir('D:/programs/lianxi/douyin/json/')  # 获取文件夹内所有json包名
+count = 1
+douyin_data = pymysql.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="root",
+    password="Qilei990406!",
+    charset="utf8",
+    database="douyindata"
+)
+cursor = douyin_data.cursor()
+for videos in videos_list:
+    a = open('D:/programs/lianxi/douyin/json/{}'.format(videos), encoding='utf-8')  # 打开json包
+    content = json.load(a)['aweme_list']  # 取出json包中所有视频
+    for video in content:  # 循环视频列表，选取每个视频
+        video_url = video['video']['play_addr']['url_list'][2]  # 获取视频url，每个视频有4个url，我选的第3个
+        video_url = video_url.replace('https', 'http')
+        video_ID = video['aweme_id']  # 获取视频内部ID
+        video_getTime = str(time.time())[:10]
+        # print(video_ID)
+        # print("now link is " + str(count))
+        count += 1
+        sql = 'insert into douyindata.videos (ID,Url,addTime) values ({},\'{}\',{})'.format(video_ID, video_url,video_getTime)
+        # print(sql)
+        # cursor.execute(sql)
+        cursor.connection.commit()
+        try:
+            cursor.execute(sql)
+
+        except:
+            print(video_ID+'重复')
+            pass
+cursor.connection.commit()
+cursor.close()
+douyin_data.close()
+
--- download.py
+++ download.py
@@ -1,8 +1,9 @@
 import os, json, requests
+import pymysql
 
 
-# 请求头
 def download():
+    # 请求头
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/72.0.3626.119 Safari/537.36'}
 
@@ -15,11 +16,16 @@ def download():
         content = json.load(a)['aweme_list']  # 取出json包中所有视频
 
         for video in content:  # 循环视频列表，选取每个视频
-            video_url = video['video']['play_addr']['url_list'][2]  # 获取视频url，每个视频有4个url，我选的第2个
+            video_url = video['video']['play_addr']['url_list'][2]  # 获取视频url，每个视频有4个url，我选的第3个
             video_url = video_url.replace('https', 'http')
+            # print(video_url)
             videoMp4 = requests.get(video_url, headers=headers).content  # 获取视频二进制代码
+            # print('video loads success {}'.format(count))
             with open('D:/programs/lianxi/douyin/videos/{}.mp4'.format(count), 'wb') as f:  # 以二进制方式写入路径，记住要先创建路径
                 f.write(videoMp4)  # 写入
                 print('视频{}下载完成'.format(count))  # 下载提示
             count += 1  # 计数+1
     return
+
+
+download()
