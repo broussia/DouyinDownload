6736bf5e53a10f1d828a28443d3f023533371ed3
QiLei <8971460+qilei123@user.noreply.gitee.com>
2021-12-22 10:39:39 +0800

    little fix

--- database.py
+++ database.py
@@ -21,10 +21,10 @@ def save_in_database():
         content = json.load(a)['aweme_list']  # 取出json包中所有视频
         for video in content:  # 循环视频列表，选取每个视频
             video_url = video['video']['play_addr']['url_list'][2]  # 获取视频url，每个视频有4个url，我选的第3个
-            video_url = video_url.replace('https', 'http')
             video_ID = video['aweme_id']  # 获取视频内部ID
-            video_getTime = str(time.time())[:10]
-            video_title = video['desc']
+            video_getTime = str(time.time())[:10] # 视频保存的时间
+            video_title = video['desc'] # 视频的标题
+            video_url = video_url.replace('https', 'http')
             # print(video_title)
             # print("now link is " + str(count))
             print(count)
